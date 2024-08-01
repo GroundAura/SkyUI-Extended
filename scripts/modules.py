201a7b39464541a8239add0b9fff37191312fb19
GroundAura <52499897+GroundAura@users.noreply.github.com>
2024-08-01 02:03:30 -0600

    Initial commit

---
+++ scripts/modules.py
@@ -0,0 +1,32 @@
+#import configparser
+#import os
+#import pathlib
+#import shutil
+#import subprocess
+#from os import getcwd as os_getcwd, path as os_path
+#from pathlib import Path
+#from subprocess import run as cmd
+#from subprocess import CalledProcessError as cmd_error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def test():
+	print("test")
+
+def main():
+	test()
+
+if __name__ == "__main__":
+	main()
